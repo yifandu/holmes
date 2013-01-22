0a262253cf31a07eea28aec8b838ba5725c4f5b6
IVAN <sd739898@163.com>
2013-01-22 14:14:41 +0800

    mainprocess

---
+++ main_process/getip_fromlog_3.py
@@ -0,0 +1,21 @@
+#-*- coding:utf-8 -*-
+
+import re,time
+def mail_log(file_path):
+        global count
+        log=open(file_path,'r')
+        C=r'(?<=[\s])(?:\d{1,3}\.){3}\d{1,3}(?=[\s])'
+        find=re.compile(C)
+        count={}
+        for i in log:
+            for ip in find.findall(i):
+                count[ip]=count.get(ip,0)+1
+if __name__ == '__main__':
+
+        mail_log(r'access.log')
+        R=count.items()
+        for i in R:
+            if i[1]>0: #提取出现次数大于0的IP
+                print i[0]
+
+
